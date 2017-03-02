6dcbc656b5bbda99cc02775641c72b2aebe230c1
ilandy <w9874123@gmail.com>
2017-03-02 17:10:24 +0800

    1. Case Interface 2. 加入 RXJS 3. 建立 report.service.ts 4. 加入假資料 data-caseitems.json

--- solution/src/app/report/report.component.ts
+++ solution/src/app/report/report.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { CaseType, SubCaseType } from '../share/case';
+import { ReportService } from './report.service';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
+  providers: [ReportService],
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  caseTypes: CaseType[];
+  error: any;
+  selectedCaseType: CaseType;
+  selectedSubCaseType: SubCaseType;
 
-  constructor() { }
+  constructor(private titleService: Title, private reportService: ReportService) { }
   
-  items = [1,2,3,4,5,6,7,8,8,9,0,1,2,3,4,5,6,7,8,9,0];
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+  getTypes() {
+    this.reportService
+      .getTypes()
+      .subscribe(types => this.caseTypes = types);
+  }
 
   ngOnInit() {
+    this.setTitle('案件陳情 - 嘉義市政府線上陳情服務平台');
+    this.getTypes();
   }
 
 }
---
+++ solution/src/app/report/report.service.ts
@@ -0,0 +1,40 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import '../share/rxjs-operators';
+import { CaseType }       from '../share/case';
+
+@Injectable()
+export class ReportService {
+  private readonly baseApiUrl = '/assets/data/';
+  private reportTypesUrl = this.baseApiUrl + 'data-caseItems.json';
+//   private validationCodeUrl = this.baseApiUrl + 'ValidationCode/';//'../../assets/vd.json';
+
+  constructor(private http: Http) { }
+
+  getTypes() : Observable<CaseType[]> {
+    return this.http.get(this.reportTypesUrl)
+               .map((res: Response) => res.json())
+               .catch(this.handleError);
+  }
+
+  getType(id: string) : Observable<CaseType> {
+    return this.http.get(this.reportTypesUrl + id)
+               .map((res: Response) => res.json()[0])
+               .catch(this.handleError);
+  }
+
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
+
+export interface RecaptchaCode {
+    HashCode: string;
+    TimeStamp: string;
+    ValidationCode: string;
+}
