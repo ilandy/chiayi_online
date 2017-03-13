a5198480452f0d3b00fb25dbd1fea4a11c4745e3
ilandy <w9874123@gmail.com>
2017-03-13 12:07:59 +0800

    FAQ 列表畫面完成

--- solution/src/app/faq/faq.component.ts
+++ solution/src/app/faq/faq.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { FaqService } from './faq.service';
+import { Category } from './faq';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.scss'],
+  providers: [FaqService]
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private faqService: FaqService, private route: ActivatedRoute) { }
+  categories: Category[] = [];
+  error: any;
+  selectValue:string = '全部';
+  selectSwitch:boolean = false;
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+  
+  getSelectItem () {
+    this.selectSwitch = true;
+  }
+
+  getCategories() {
+    this.faqService
+        .getCategories()
+        .subscribe(
+          category => this.categories = category,
+          error => this.error = error);
+  }
 
   ngOnInit() {
+    this.setTitle('常見問題 FAQ - 嘉義市政府線上陳情服務平台');
+    this.getCategories();
   }
 
 }
